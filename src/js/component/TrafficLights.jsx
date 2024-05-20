55647488cd681fbf4fcd8e90037855665a4d8e34
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-05-20 16:53:53 +0000

    Fixed traffice lights (now working properly)

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -4,20 +4,14 @@ const TrafficLights = () => {
 
     const [color, setColor] = useState(null)
 
-    const light = document.getElementById('#trafficTop')
-
-    if (color === "red"){
-        return light.style.boxShadow ="0 0 100px white";
-    }
-
   return (
     <div>
       <div id="lightrod"></div>
       <div id="lightCavin">
         <div className="py-3">
-            <button id="trafficTop" onClick={() => setColor("red")}></button>
-            <button id="trafficMiddle" onClick={() => setColor("yellow")}></button>
-            <button id="trafficBottom" onClick={() => setColor("green")}></button>
+            <button id="trafficTop" className={color === "red" ? "glow" : ""} onClick={() => setColor("red")}></button>
+            <button id="trafficMiddle" className={color ==="yellow" ? "glow" : ""}onClick={() => setColor("yellow")}></button>
+            <button id="trafficBottom" className={color==="green" ? "glow": ""}onClick={() => setColor("green")}></button>
         </div>
       </div>
     </div>
